0138bd27a9c0fad4e354a0326def11a3ebb5afed
Leo <5437289+cross44@users.noreply.github.com>
2020-05-26 17:58:01 +0300

    1 org - 1 branch

--- .github/actions/cleanup-cf/entrypoint.sh
+++ .github/actions/cleanup-cf/entrypoint.sh
@@ -8,16 +8,16 @@ INSTALL_TIMEOUT=40 #service deploy timeout
 branch_name=$(echo $GITHUB_REF | awk -F'/' '{print $3}')
 #branch_name=$(echo $GITHUB_REF | awk -F'\' '{print $4}') #TODO windows
 echo $branch_name
-org_name="atlas-test-org"
+org_name="atlas-test-$branch_name"
 make_pcf_metadata $INPUT_PCF_URL $INPUT_PCF_USER $INPUT_PCF_PASSWORD
 
 cf_login
-space_name="test-space"-$branch_name
-cf target -o $org_name -s $space_name
+cf target -o $org_name -s $org_name
 
 cf unbind-service test-app-$branch_name aws-atlas-test-instance-$branch_name
 cf delete-service aws-atlas-test-instance-$branch_name -f
 wait_service_status_change aws-atlas-test-instance-$branch_name "delete in progress"
 cf delete-service-broker mongodb-atlas-$branch_name -f
 delete_application test-app-$branch_name
+delete_application simple-app-$branch_name
 delete_application atlas-osb-app-$branch_name
