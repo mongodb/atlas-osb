4a613b74cf9c5b7512b56ccebfca3d862e1bba7e
Leo <5437289+cross44@users.noreply.github.com>
2020-05-26 12:44:02 +0300

    simple test

--- .github/actions/prepare-cf/entrypoint.sh
+++ .github/actions/prepare-cf/entrypoint.sh
@@ -26,26 +26,35 @@ cf marketplace
 
 create_service aws-atlas-test-instance-$branch_name
 
-echo "Prepare app"
-#we can hide "prepare app" with
-#cf push APP_NAME --docker-image [REGISTRY_HOST:PORT/]IMAGE[:TAG] [--docker-username USERNAME] [-c COMMAND] [-f MANIFEST_PATH | --no-manifest] [--no-start] [-i NUM_INSTANCES] [-k DISK] [-m MEMORY] [-t HEALTH_TIMEOUT] [-u (process | port | http)] [--no-route | --random-route | --hostname HOST | --no-hostname] [-d DOMAIN] [--route-path ROUTE_PATH] [--var KEY=VALUE]... [--vars-file VARS_FILE_PATH]...
-git clone https://github.com/cloudfoundry-samples/spring-music.git
-cd spring-music
-./gradlew clean assemble
-cf push test-app-$branch_name --no-start
-cf set-env test-app-$branch_name JAVA_OPTS "-Dspring.profiles.active=mongodb"
-cf bind-service test-app-$branch_name aws-atlas-test-instance-$branch_name
-
-u=$(cf env test-app-$branch_name | awk '$1 ~ /"uri"\:/{print substr($2, 16, length($2)-17) }')
-uname=$(cf env test-app-$branch_name | awk '$1 ~ /username/{print substr($2, 2, length($2)-3) }')
-p=$(cf env test-app-$branch_name | awk '$1 ~ /password/{print substr($2, 2, length($2)-3) }')
-db="test"
-connection="\"mongodb+srv://$uname:$p@$u/$db\""
-awk '/generate-ddl: true/ { print; print "  data:"; print "    mongodb:"; print "      uri: "'"${connection}"'""; next }1' src/main/resources/application.yml > application_temp | rm src/main/resources/application.yml | mv application_temp src/main/resources/application.yml
-
-./gradlew clean assemble
-cf push test-app-$branch_name --no-start
-cf env test-app-$branch_name
-cf restart test-app-$branch_name
-app_url=$(cf app test-app-$branch_name | awk '$1 ~ /routes:/{print $2}')
+echo "Simple app"
+git clone https://github.com/leo-ri/simple-ruby.git
+cf push simple-app-$branch_name --no-start
+cf bind-service simple-app-$branch_name aws-atlas-test-instance-$branch_name
+cf restage simple-app-$branch_name
+app_url=$(cf app simple-app-$branch_name | awk '$1 ~ /routes:/{print $2}')
 echo "::set-output name=app_url::$app_url"
+
+
+#echo "Prepare app"
+#we can hide "prepare app" with
+# #cf push APP_NAME --docker-image [REGISTRY_HOST:PORT/]IMAGE[:TAG] [--docker-username USERNAME] [-c COMMAND] [-f MANIFEST_PATH | --no-manifest] [--no-start] [-i NUM_INSTANCES] [-k DISK] [-m MEMORY] [-t HEALTH_TIMEOUT] [-u (process | port | http)] [--no-route | --random-route | --hostname HOST | --no-hostname] [-d DOMAIN] [--route-path ROUTE_PATH] [--var KEY=VALUE]... [--vars-file VARS_FILE_PATH]...
+# git clone https://github.com/cloudfoundry-samples/spring-music.git
+# cd spring-music
+# # ./gradlew clean assemble
+# # cf push test-app-$branch_name --no-start
+# # cf set-env test-app-$branch_name JAVA_OPTS "-Dspring.profiles.active=mongodb"
+# # cf bind-service test-app-$branch_name aws-atlas-test-instance-$branch_name
+
+# u=$(cf env test-app-$branch_name | awk '$1 ~ /"uri"\:/{print substr($2, 16, length($2)-17) }')
+# uname=$(cf env test-app-$branch_name | awk '$1 ~ /username/{print substr($2, 2, length($2)-3) }')
+# p=$(cf env test-app-$branch_name | awk '$1 ~ /password/{print substr($2, 2, length($2)-3) }')
+# db="test"
+# connection="\"mongodb+srv://$uname:$p@$u/$db\""
+# awk '/generate-ddl: true/ { print; print "  data:"; print "    mongodb:"; print "      uri: "'"${connection}"'""; next }1' src/main/resources/application.yml > application_temp | rm src/main/resources/application.yml | mv application_temp src/main/resources/application.yml
+
+# ./gradlew clean assemble
+# cf push test-app-$branch_name --no-start
+# cf env test-app-$branch_name
+# cf restart test-app-$branch_name
+# app_url=$(cf app test-app-$branch_name | awk '$1 ~ /routes:/{print $2}')
+# echo "::set-output name=app_url::$app_url"
