b1ec35d8be0429b31efee1ca0429b24b2865889b
Leo <5437289+cross44@users.noreply.github.com>
2020-06-23 11:10:19 +0300

    postfix - branch+commit_id, use params.sh in prepare-cf

--- .github/actions/cleanup-cf/entrypoint.sh
+++ .github/actions/cleanup-cf/entrypoint.sh
@@ -5,11 +5,11 @@ source ".github/base-dockerfile/helpers/cf-helper.sh"
 source ".github/base-dockerfile/helpers/params.sh"
 
 echo "CleanUP: delete service broker, service, unbind app"
-INSTALL_TIMEOUT=40 #service deploy timeout
+INSTALL_TIMEOUT=30 #service deploy timeout
 
 make_pcf_metadata $INPUT_PCF_URL $INPUT_PCF_USER $INPUT_PCF_PASSWORD
 
-cf_login $ORG_NAME $ORG_NAME
+cf_login $ORG_NAME $SPACE_NAME
 
 delete_service_app_if_exists $SERVICE_ATLAS $TEST_SIMPLE_APP
 cf delete-service-broker $BROKER -f
@@ -17,3 +17,4 @@ delete_application $TEST_SPRING_APP
 delete_application $TEST_SIMPLE_APP
 delete_application $BROKER_APP
 cf delete-service $CREDHUB -f
+cf delete-space $SPACE_NAME -f
--- .github/actions/prepare-cf/entrypoint.sh
+++ .github/actions/prepare-cf/entrypoint.sh
@@ -2,35 +2,35 @@
 
 source ".github/base-dockerfile/helpers/tmp-helper.sh"
 source ".github/base-dockerfile/helpers/cf-helper.sh"
+source ".github/base-dockerfile/helpers/params.sh"
 
 echo "Prepare CF env for testing"
 
 echo "init"
 INSTALL_TIMEOUT=40 #service deploy timeout
 branch_name=$(echo $GITHUB_REF | awk -F'/' '{print $3}')
-org_name="atlas-test-$branch_name"
 make_pcf_metadata $INPUT_PCF_URL $INPUT_PCF_USER $INPUT_PCF_PASSWORD
 
 echo "Login. Create ORG and SPACE depended on the branch name"
 cf_login
-cf create-org $org_name && cf target -o $org_name
-cf create-space $org_name && cf target -s $org_name
+cf create-org $ORG_NAME && cf target -o $ORG_NAME
+cf create-space $SPACE_NAME && cf target -s $SPACE_NAME
 
 echo "Create service-broker"
-create_atlas_service_broker_from_repo mongodb-atlas-$branch_name atlas-osb-app-$branch_name
+create_atlas_service_broker_from_repo $BROKER $BROKER_APP
 
 cf marketplace
 
-create_service aws-atlas-test-instance-$branch_name
+create_service $SERVICE_ATLAS "M10"
 
 echo "Simple app"
 git clone https://github.com/leo-ri/simple-ruby.git
 cd simple-ruby
-cf push simple-app-$branch_name --no-start
-cf bind-service simple-app-$branch_name aws-atlas-test-instance-$branch_name
-cf restart simple-app-$branch_name
-check_app_started simple-app-$branch_name
-app_url=$(cf app simple-app-$branch_name | awk '$1 ~ /routes:/{print $2}')
+cf push $TEST_SIMPLE_APP --no-start
+cf bind-service $TEST_SIMPLE_APP $SERVICE_ATLAS
+cf restart $TEST_SIMPLE_APP
+check_app_started $TEST_SIMPLE_APP
+app_url=$(cf app $TEST_SIMPLE_APP | awk '$1 ~ /routes:/{print $2}')
 echo "::set-output name=app_url::$app_url"
 
 #echo "Prepare app"
