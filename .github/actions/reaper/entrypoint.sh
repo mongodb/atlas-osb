e4ff5dc674069bf7a7a52d68d4d1185575798d97
Leo <5437289+cross44@users.noreply.github.com>
2020-07-21 17:17:04 +0300

    ci: cleanup atlas organization

---
+++ .github/actions/reaper/entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Will delete PROJECT AND CLUSTER IN ORGANIZATION except M0 and project ValleyOfTesing
+# doesn't wait cluster termination for deleting project
+
+set -e
+source ".github/base-dockerfile/helpers/atlasAPI.sh"
+
+projects=$(get_projects)
+
+for elkey in $(echo "$projects" | jq '.results | keys | .[]'); do
+    element=$(echo "$projects" | jq ".results[$elkey]")
+    count=$(echo $element | jq -r '.clusterCount')
+    id=$(echo $element | jq -r '.id')
+    orgID=$(echo $element | jq -r '.orgID')
+    name=$(echo $element | jq -r '.name')
+    deleteable=true
+
+    if [[ $count != 0 ]]; then
+        clusters=$(get_clusters $id)
+
+        #check cluster size, if it is not M0 - delete.
+        for ckey in $(echo $clusters | jq '.results | keys | .[]'); do
+            cluster=$(echo $clusters | jq -r ".results[$ckey]")
+            csize=$(echo "$cluster" | jq -r '.providerSettings.instanceSizeName')
+            cname=$(echo "$cluster" | jq -r '.name')
+            if [[ $csize != "M0" ]]; then
+                echo "delete cluster: $id $cname $csize"
+                delete_cluster $id $cname
+                #not going to wait for deleting projects
+            else 
+                echo "$cname $csize is M0"
+            fi
+        done
+    else 
+        if [[ $name != "ValleyOfTesting" ]]; then
+            echo "deleting project: $id"
+            delete_project $id
+        else
+            echo "won't delete ValleyOfTesting"
+        fi
+    fi
+done 
+
+
---
+++ .github/base-dockerfile/helpers/atlasAPI.sh
@@ -0,0 +1,32 @@
+#ATLAS API documentation https://docs.atlas.mongodb.com/reference/api/projects/ 
+
+BASE_URL="https://cloud.mongodb.com/api/atlas/v1.0"
+
+get_projects() {
+    curl -s -u "${INPUT_PUBLIC_KEY}:${INPUT_PRIVATE_KEY}" --digest "${BASE_URL}/groups"
+}
+
+get_clusters() {
+    projectID=$1
+    curl -s -u "${INPUT_PUBLIC_KEY}:${INPUT_PRIVATE_KEY}" --digest "${BASE_URL}/groups/${projectID}/clusters"
+}
+
+delete_cluster() {
+    projectID=$1
+    name=$2
+    echo "${BASE_URL}/groups/${projectID}/clusters/${name}"
+    curl -s -u "${INPUT_PUBLIC_KEY}:${INPUT_PRIVATE_KEY}" --digest -X DELETE "${BASE_URL}/groups/${projectID}/clusters/${name}"
+}
+delete_project() {
+    projectID=$1
+    curl -s -X DELETE --digest -u "${INPUT_PUBLIC_KEY}:${INPUT_PRIVATE_KEY}" "${BASE_URL}/groups/${projectID}"
+}
+
+get_cluster_info() {
+    projectID=$1
+    name=$2
+    curl -s -u "${INPUT_PUBLIC_KEY}:${INPUT_PRIVATE_KEY}" --digest \
+     --header "Content-Type: application/json" \
+     --include \
+     --request GET "${BASE_URL}/groups/${projectID}/clusters/${name}?pretty=true"
+}
