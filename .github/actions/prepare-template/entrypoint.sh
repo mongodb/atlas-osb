a2e34062617393b7ec1c9fee1e47c0510c3e6dfe
Leo <5437289+cross44@users.noreply.github.com>
2020-07-28 17:30:26 +0300

    add BROKER_APIKEYS as a file

--- .github/actions/prepare-template/entrypoint.sh
+++ .github/actions/prepare-template/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd atlas-osb
 
 source ".github/base-dockerfile/helpers/tmp-helper.sh"
 source ".github/base-dockerfile/helpers/cf-helper.sh"
@@ -9,7 +8,7 @@ echo "init"
 INSTALL_TIMEOUT=40 #service deploy timeout
 branch_name=$(echo $GITHUB_REF | awk -F'/' '{print $3}')
 make_pcf_metadata $INPUT_PCF_URL $INPUT_PCF_USER $INPUT_PCF_PASSWORD
-make_multikey_config ./apikeys-config.json
+make_multikey_config samples/apikeys-config.json
 
 echo "Login. Create ORG and SPACE depended on the branch name"
 cf_login
@@ -17,16 +16,10 @@ cf create-org $ORG_NAME && cf target -o $ORG_NAME
 cf create-space $SPACE_NAME && cf target -s $SPACE_NAME
 
 echo "Create service-broker"
-cf push $BROKER_APP --no-start
-# cf set-env $BROKER_APP BROKER_HOST 0.0.0.0
-# cf set-env $BROKER_APP BROKER_PORT 8080
-cf set-env $BROKER_APP BROKER_APIKEYS $(awk NF=NF RS= OFS= < apikeys-config.json)
-# cf set-env $BROKER_APP ATLAS_BROKER_TEMPLATEDIR ./samples/plans
-# cf set-env $BROKER_APP BROKER_OSB_SERVICE_NAME $BROKER_OSB_SERVICE_NAME 
-
-cf start $BROKER_APP
+cf push $BROKER_APP
 check_app_started $BROKER_APP
 app_url=$(cf app $BROKER_APP | awk '/routes:/{print $2}')
+
 cf create-service-broker $BROKER "admin" "admin" http://$app_url --space-scoped #TODO form
 
 cf marketplace
