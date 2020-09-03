655c212abed09b7d28dde5112475d2403a44a32b
Leo <5437289+cross44@users.noreply.github.com>
2020-09-03 12:10:27 +0300

    rename service

--- .github/base-dockerfile/helpers/params.sh
+++ .github/base-dockerfile/helpers/params.sh
@@ -1,7 +1,7 @@
 #shellcheck shell=bash disable=SC2034
 
 branch_name=$(echo "$GITHUB_REF" | awk -F'/' '{print $3}')
-branch_name=${branch_name:0:30} #service name max length is 50 symbols minus prefixes
+branch_name=${branch_name:0:26} #service name max length is 50 symbols minus prefixes
 branch_name=$(echo "${branch_name}" | tr "." "-")
 commit_id=$(git rev-parse --short HEAD)
 postfix=$branch_name-$commit_id
@@ -15,5 +15,6 @@ CREDHUB=credhub-$postfix
 TEST_SIMPLE_APP=simple-app-$postfix
 TEST_SPRING_APP=music-$postfix
 SERVICE_ATLAS=instance-$postfix
+SERVICE_ATLAS_RENAME=$SERVICE_ATLAS-rnm
 
 BROKER_OSB_SERVICE_NAME="atlas"
