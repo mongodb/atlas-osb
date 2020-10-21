f1594e625f5e0cef932c720a61ab930c612ec970
Jason Mimick <jmimick@gmail.com>
2020-10-21 07:32:11 -0400

    fix sample key, fix shellcheck fail

--- run-broker-locally.sh
+++ run-broker-locally.sh
@@ -4,10 +4,11 @@
 export BROKER_LOG_LEVEL=DEBUG
 export BROKER_HOST=0.0.0.0
 export BROKER_PORT=4000
-export $(grep -v '#.*' ~/local.env | xargs) && envsubst < ./samples/apikeys-config.json.sample > ./keys
+export "$(grep -v "#.*" ~/local.env | xargs)" && envsubst < ./samples/apikeys-config.json.sample > ./keys
 export BROKER_APIKEYS=./keys
 export ATLAS_BROKER_TEMPLATEDIR=$(pwd)/samples/plans
 tree "${ATLAS_BROKER_TEMPLATEDIR}"
 env | grep BROKER
+cat keys
 
 ./atlas-osb
