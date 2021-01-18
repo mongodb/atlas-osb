8251a8e59007b451683c6576fe7387cd89549f47
Pavel <explosere@gmail.com>
2021-01-18 15:24:37 +0300

    Add Integrations

--- pkg/broker/dynamicplans/plan.go
+++ pkg/broker/dynamicplans/plan.go
@@ -23,16 +23,18 @@ import (
 
 // Plan represents a set of MongoDB Atlas resources
 type Plan struct {
-	Version       string                              `json:"version,omitempty"`
-	Name          string                              `json:"name,omitempty"`
-	Description   string                              `json:"description,omitempty"`
-	Free          *bool                               `json:"free,omitempty"`
-	APIKey        *credentials.APIKey                 `json:"apiKey,omitempty"`
-	Project       *mongodbatlas.Project               `json:"project,omitempty"`
-	Cluster       *mongodbatlas.Cluster               `json:"cluster,omitempty"`
-	DatabaseUsers []*mongodbatlas.DatabaseUser        `json:"databaseUsers,omitempty"`
-	IPAccessLists []*mongodbatlas.ProjectIPAccessList `json:"ipAccessLists,omitempty"`
-	Settings      map[string]interface{}              `json:"settings,omitempty"`
+	Version       string                                `json:"version,omitempty"`
+	Name          string                                `json:"name,omitempty"`
+	Description   string                                `json:"description,omitempty"`
+	Free          *bool                                 `json:"free,omitempty"`
+	APIKey        *credentials.APIKey                   `json:"apiKey,omitempty"`
+	Project       *mongodbatlas.Project                 `json:"project,omitempty"`
+	Cluster       *mongodbatlas.Cluster                 `json:"cluster,omitempty"`
+	DatabaseUsers []*mongodbatlas.DatabaseUser          `json:"databaseUsers,omitempty"`
+	IPAccessLists []*mongodbatlas.ProjectIPAccessList   `json:"ipAccessLists,omitempty"`
+	Integrations  []*mongodbatlas.ThirdPartyIntegration `json:"integrations,omitempty"`
+
+	Settings map[string]interface{} `json:"settings,omitempty"`
 
 	// Deprecated: Use IPAccessLists instead!
 	IPWhitelists []*mongodbatlas.ProjectIPWhitelist `json:"ipWhitelists,omitempty"`
