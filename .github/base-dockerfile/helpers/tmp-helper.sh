7823ce4d3867c243f3ae46c37d779080a52e06d5
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2021-06-29 20:35:04 +0300

    move make metadata inside login, !docs

--- .github/base-dockerfile/helpers/tmp-helper.sh
+++ .github/base-dockerfile/helpers/tmp-helper.sh
@@ -23,20 +23,3 @@ make_multikey_config() {
 
 EOF
 }
-
-make_pcf_metadata() {
-	local PCF_URL=$1
-	local PCF_USERNAME=$2
-	local PCF_PASSWORD=$3
-	file="metadata"
-	if [ -f $file ]; then
-		rm $file
-	fi
-	cat >$file <<EOF
----
-opsmgr:
-  url: "${PCF_URL}"
-  username: "${PCF_USERNAME}"
-  password: "${PCF_PASSWORD}"
-EOF
-}
